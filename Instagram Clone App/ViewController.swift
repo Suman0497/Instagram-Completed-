a2e7bd1fa16d9b14406b50e5443a6f35250eadf4
Suman <sumanreshmj@gmail.com>
2021-10-13 19:31:27 +0530

    Initial Commit

---
+++ Instagram Clone App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram Clone App
+//
+//  Created by apple on 13/10/21.
+//  Copyright © 2021 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
